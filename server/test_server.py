4593f3096325bba9b379587075bf16ac93e3b881
None <None>
2017-11-10 22:07:11 -0800

    tests passing

--- server/test_server.py
+++ server/test_server.py
@@ -22,7 +22,9 @@ class TestApiEndpoints(unittest.TestCase):
         self.server_thread.setDaemon(True)
         self.server_thread.start()
         # Wait a bit for the server to come up
-        time.sleep(0.2)
+        time.sleep(0.5)
+        # make sure its empty
+        self.assertEqual(len(self.bbserver.get_all_entries()), 0)
 
     def tearDown(self):
         # Call after every test case
@@ -36,19 +38,19 @@ class TestApiEndpoints(unittest.TestCase):
         self.assertEqual(r.json(), {})
 
     def test_posted_entry_should_exist(self):
-        r1 = requests.post(BASE_URL + '/board', data={'entry': 'banana'})
+        r1 = requests.post(BASE_URL + '/board', data={'entry': 'banana-a'})
         self.assertEqual(r1.status_code, 200)
         posted_id = r1.json()['id']
         self.assertNotEqual(posted_id, None)
 
         r2 = requests.get(BASE_URL + '/board')
         self.assertEqual(r2.status_code, 200)
-        self.assertEqual(r2.json()[posted_id], 'banana')
+        self.assertEqual(r2.json()[posted_id], 'banana-a')
 
     def test_delete_entry(self):
         # Post two entries
-        r1 = requests.post(BASE_URL + '/board', data={'entry': 'apple'})
-        r2 = requests.post(BASE_URL + '/board', data={'entry': 'banana'})
+        r1 = requests.post(BASE_URL + '/board', data={'entry': 'apple-b'})
+        requests.post(BASE_URL + '/board', data={'entry': 'banana-b'})
         apple_id = r1.json()['id']
         # first delete should be successful
         r3 = requests.delete(BASE_URL + '/entries/' + apple_id)
@@ -56,6 +58,21 @@ class TestApiEndpoints(unittest.TestCase):
         # deleting it again should not be.
         r4 = requests.delete(BASE_URL + '/entries/' + apple_id)
         self.assertEqual(r4.status_code, 404)
+        # Server should contain 1 entry
+        self.assertEqual(len(self.bbserver.get_all_entries()), 1)
+
+    def test_modify_entry(self):
+        # Post entry
+        r1 = requests.post(BASE_URL + '/board', data={'entry': 'apple-c'})
+        mod_id = r1.json()['id']
+        # modify should be successful
+        r2 = requests.post(BASE_URL + '/entries/' + mod_id, data={'entry': "apple-c-updated"})
+        self.assertEqual(r2.status_code, 200)
+        # check the modified entry value
+        r3 = requests.get(BASE_URL + '/board')
+        self.assertEqual(r3.json()[mod_id], 'apple-c-updated')
+        # Server should contain 1 entry
+        self.assertEqual(len(self.bbserver.get_all_entries()), 1)
 
 if __name__ == '__main__':
     unittest.main()  # run all tests
