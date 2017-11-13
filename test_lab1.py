8446f106e8c4e0321426c962f364ba478d50c117
Peter Pickerill <peterpickerill2016@gmail.com>
2017-11-13 23:38:41 +0100

    Added new Unit Tests

--- test_lab1.py
+++ test_lab1.py
@@ -2,6 +2,8 @@ import unittest
 import requests
 import json
 
+POST_ID = None
+
 class SimpleTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -10,18 +12,35 @@ class SimpleTestCase(unittest.TestCase):
     def tearDown(self):
         """Call after every test case."""
 
-    def testA(self):
-        # Posting an entry to board check if it exists:
+    def testBoard(self):
         r = requests.get("http://10.1.0.1:61001/board")
         assert r.status_code == 200
-
+    def testEntryAndModify(self):
+        ENTRY_1 = "banana"
+        r = requests.post("http://10.1.0.1:61001/board", data={'entry': ENTRY_1})
+        data = json.loads(r.content)
+        POST_ID = data.id
+        assert r.status_code == 200
+        #modify
+        ENTRY_1 = "apple"
+        r = requests.post("http://10.1.0.1:61001/board" + "/" + POST_ID, data={'entry': ENTRY_1})
+        data = json.loads(r.content)
+        assert r.status_code == 200
+    def testEntry(self):
+        # Posting an entry to board check if it exists:
         ENTRY_1 = "banana"
         r = requests.post("http://10.1.0.1:61001/board", data={'entry': ENTRY_1})
         assert r.status_code == 200
-
-        r = requests.get("http://10.1.0.1:61001/board")
+    def testDelete(self):
+        ENTRY_1 = "banana"
+        r = requests.post("http://10.1.0.1:61001/board", data={'entry': ENTRY_1})
         data = json.loads(r.content)
-        print "r: %s" % data
-
+        POST_ID = data.id
+        assert r.status_code == 200
+        #modify
+        ENTRY_1 = "apple"
+        r = requests.delete("http://10.1.0.1:61001/board" + "/" + POST_ID)
+        assert r.status_code == 200
+        
 if __name__ == "__main__":
     unittest.main() # run all tests
