864c0273506e7ff71e12c8960a8a60882e6d1327
Henrik Numé <henriknume@gmail.com>
2017-11-30 09:03:17 +0100

    removing leader elections code

--- server/server.py
+++ server/server.py
@@ -26,7 +26,7 @@ PORT_NUMBER = 61001
 #------------------------------------------------------------------------------------------------------
 
 class BlackboardServer(HTTPServer):
-#------------------------------------------------------------------------------------------------------
+
     def __init__(self, server_address, handler, node_id, vessel_list):
     # We call the super init
         HTTPServer.__init__(self,server_address, handler)
@@ -34,49 +34,29 @@ class BlackboardServer(HTTPServer):
         self.Entries = {}
         # our own ID (IP is 10.1.0.ID)
         self.vessel_id = node_id
-        self.leader = None
-        self.failedLeaders = 0
         # The list of other vessels
         self.vessels = vessel_list
         self.identifier = str(random.random()*99999)
         self.finger_table = {self.identifier:self.get_ip_address()}
-        if len(vessel_list) > 1:
-            self.initiateElection()
-#------------------------------------------------------------------------------------------------------
+
+
     # Closes socket before shutdown so it can be reused in tests.
     def shutdown(self):
         self.socket.close()
         HTTPServer.shutdown(self)
-#------------------------------------------------------------------------------------------------------
+
+
     # We delete a value received from the store
     def delete_value_in_store(self,key):
         # we delete a value in the store if it exists
         del self.Entries[key]
 
-    def initiateElection(self):
-        thread = Thread(target=self.election)
-        # We kill the process if we kill the server
-        thread.daemon = True
-        # We start the thread
-        thread.start()
-
-    def find_neighbour(self):
-        return "%s%d" % (IP_ADDRESS_PREFIX, (self.vessel_id % len(self.vessels)+1))
 
     def get_ip_address(self):
         return IP_ADDRESS_PREFIX + str(self.vessel_id)
 
-#---------------------------------------------------------------------------------
-# LAB 2 election
-    def election(self):
-        if len(self.vessels) == 0:
-            return
-        time.sleep(1)
-        print ("Initiating Election")
-        print ("I am %s and my neighbour is %s" % (self.vessel_id, (self.vessel_id % len(self.vessels)) +1))
-        self.contact_vessel_for_election(self.find_neighbour(), "/ELECTION", "POST", "election_table", json.dumps(self.finger_table))
-#------------------------------------------------------------------------------------------------------
-# Contact a specific vessel with a set of variables to transmit to it
+
+    # Contact a specific vessel with a set of variables to transmit to it
     def contact_vessel(self, vessel_ip, path, action_type, key, value):
         # the Boolean variable we will return
         success = False
@@ -99,11 +79,6 @@ class BlackboardServer(HTTPServer):
             if status == 200:
                 success = True
         # We catch every possible exceptions
-        except TimeoutError as e:
-            self.failedLeaders = self.failedLeaders + 1
-            self.leader = self.finger_table[sorted(self.finger_table)[self.leaderNum]]
-            self.contact_vessel(self, vessel_ip, path, action_type, key, value)
-            #Here we should retransmit to the next vessel in the finger table
         except Exception as e:
             print "Error while contacting %s" % vessel_ip
             # printing the error given by Python
@@ -111,38 +86,8 @@ class BlackboardServer(HTTPServer):
 
         # we return if we succeeded or not
         return success
-    def contact_vessel_for_election(self, vessel_ip, path, action_type, key, value):
-        # the Boolean variable we will return
-        success = False
-        # The variables must be encoded in the URL format, through urllib.urlencode
-        post_content = urlencode({key:value})
-        # the HTTP header must contain the type of data we are transmitting, here URL encoded
-        headers = {"Content-type": "application/x-www-form-urlencoded"}
-        # We should try to catch errors when contacting the vessel
-        try:
-            # We contact vessel:PORT_NUMBER since we all use the same port
-            # We can set a timeout, after which the connection fails if nothing happened
-            connection = HTTPConnection("%s:%d" % (vessel_ip, PORT_NUMBER), timeout = 30)
-            # We only use POST to send data (PUT and DELETE not supported)
-            #action_type = "POST"
-            # We send the HTTP request
-            connection.request(action_type, path, post_content, headers)
-            # We retrieve the response
-            response = connection.getresponse()
-            # We want to check the status, the body should be empty
-            status = response.status
-            # If we receive a HTTP 200 - OK
-            if status == 200:
-                success = True
-        # We catch every possible exceptions
-        except Exception as e:
-            print "Error while contacting %s" % vessel_ip
-            # printing the error given by Python
-            print(e)
 
-        # we return if we succeeded or not
-        return success
-#------------------------------------------------------------------------------------------------------
+
     # We send a received value to all the other vessels of the system
     def propagate_value_to_vessels(self, path, action_type, key, value, ):
         # We iterate through the vessel list
@@ -154,7 +99,7 @@ class BlackboardServer(HTTPServer):
                 print "---> propagating to %s" % vessel
                 self.contact_vessel(vessel, path, action_type, key, value)
 
-#------------------------------------------------------------------------------------------------------
+
 #------------------------------------------------------------------------------------------------------
 # This class implements the logic when a server receives a GET or POST request
 # It can access to the server data through self.server.*
@@ -175,7 +120,8 @@ class BlackboardRequestHandler(BaseHTTPRequestHandler):
         #self.send_header("Content-type","text/html")
         # No more important headers, we can close them
         self.end_headers()
-#------------------------------------------------------------------------------------------------------
+
+
     # a POST request must be parsed through urlparse.parse_QS, since the content is URL encoded
     def parse_POST_request(self):
         post_data = ""
@@ -185,7 +131,7 @@ class BlackboardRequestHandler(BaseHTTPRequestHandler):
         post_data = parse_qs(self.rfile.read(length), keep_blank_values=1)
         # we return the data
         return post_data
-#------------------------------------------------------------------------------------------------------
+
 #------------------------------------------------------------------------------------------------------
 # Request handling - GET
 #------------------------------------------------------------------------------------------------------
@@ -198,16 +144,6 @@ class BlackboardRequestHandler(BaseHTTPRequestHandler):
         if self.path == "/board":
             self.do_GET_Board()
 
-        elif self.path == "/entries":
-            self.do_GET_Entries()
-
-        elif self.path == "/leader":
-            self.set_HTTP_headers(200)
-            if (server.leader == None):
-                self.wfile.write(json.dumps({"success":False, "reason":"No leader yet"}))
-            else:
-                self.wfile.write(json.dumps({"success":True, "leader":server.leader, "randomNumber":str(server.finger_table)}))
-
         # Default?
         else:
             self.do_GET_Index()
@@ -215,15 +151,6 @@ class BlackboardRequestHandler(BaseHTTPRequestHandler):
 #------------------------------------------------------------------------------------------------------
 # GET logic - specific path
 #------------------------------------------------------------------------------------------------------
-    def do_GET_Entries(self):
-        self.set_HTTP_headers(200)
-        self.wfile.write(self.gen_entries_html)
-
-    def gen_entries_html(self):
-        html_response = ""
-        for key, value in self.server.Entries.iteritems():
-            html_response += entry_template % ("entries/", key, value )
-        return html_response
 
     def do_GET_Board(self):
         self.set_HTTP_headers(200)
@@ -239,9 +166,6 @@ class BlackboardRequestHandler(BaseHTTPRequestHandler):
 
 
 #------------------------------------------------------------------------------------------------------
-    # we might want some other functions
-#------------------------------------------------------------------------------------------------------
-#------------------------------------------------------------------------------------------------------
 # Request handling - POST
 #------------------------------------------------------------------------------------------------------
     def do_POST(self):
@@ -250,6 +174,7 @@ class BlackboardRequestHandler(BaseHTTPRequestHandler):
         # and set the headers for the client
         request_path = self.path
         parameters = self.parse_POST_request()
+        print("Receiving a POST on %s" % self.path)
         print "parameters: %s" % parameters
         self.set_HTTP_headers(200)
         if request_path == "/board":
@@ -285,14 +210,6 @@ class BlackboardRequestHandler(BaseHTTPRequestHandler):
                 # pass along post to leader
                 self.server.contact_vessel(self.server.leader, "/board", "POST", id, entry)
 
-        #  Cost of post delivered to all nodes:
-        #
-        #   Upon a post event to a vessel:
-        #   first retransmit to leader. (1)
-        #   (all messages contain only one entry i.e payload = 1)
-        #   then propagation from leader to all other vessels. (N-1)
-        #   Cost for post = N = number of vessels
-
         elif request_path == "/propagate/board":
             id = parameters['id'][0]
             self.server.Entries[id] = json.loads(parameters['entry'][0])
@@ -303,13 +220,6 @@ class BlackboardRequestHandler(BaseHTTPRequestHandler):
             self.server.Entries[id] = parameters['entry'][0]
             self.success_out()
 
-        elif request_path == ("/ELECTION"):
-            print "/ELECTION endpoint hit"
-            thread = Thread(target=self.election,args=([parameters["election_table"][0]]))
-            # We kill the process if we kill the server
-            thread.daemon = True
-            # We start the thread
-            thread.start()
 
     def success_out(self):
             self.set_HTTP_headers(200)
@@ -330,31 +240,8 @@ class BlackboardRequestHandler(BaseHTTPRequestHandler):
             # We start the thread
             thread.start()
 
-    def election(self, their_finger_table_string):
-            their_finger_table = json.loads(their_finger_table_string)
-            if server.identifier in their_finger_table.keys():
-                print ("Election over")
-                server.finger_table = their_finger_table
-                self.server.failedLeaders = 0
-                server.leaderRandomNumber = sorted(their_finger_table)[0]
-                server.leader = their_finger_table[sorted(their_finger_table)[0]]
-                print ("The leader is %s" % server.leader)
-                #when our own id is in the fingertable we can assume that the election has
-                # reached all nodes (e.g. gone full circle, one round)
-                # We then select the leader with the lowest key
-            else:
-                print ("Sending Vote")
-                their_finger_table[server.identifier] = server.get_ip_address()
-                server.contact_vessel_for_election(server.find_neighbour(), "/ELECTION", "POST", "election_table", json.dumps(their_finger_table))
-
 
 #------------------------------------------------------------------------------------------------------
-# POST Logic
-#------------------------------------------------------------------------------------------------------
-    # We might want some functions here as well
-#------------------------------------------------------------------------------------------------------
-
-
 # Request handling - DELETE
 #------------------------------------------------------------------------------------------------------
     def do_DELETE(self):
@@ -398,7 +285,7 @@ class BlackboardRequestHandler(BaseHTTPRequestHandler):
                 print ("Propagate!: ID NOT FOUND")
                 self.error_out("Not found", 404)
 
-#---------------------------------------------------------------------------------
+
 # file i/o
 def read_file(filename):
     curr_path = sys.path[0]
@@ -408,7 +295,7 @@ def read_file(filename):
         all_content += line
     opened_file.close()
     return all_content
-#------------------------------------------------------------------------------------------------------
+
 
 def return_entry_timestamp(entry):
     if 'timestamp' not in entry:
