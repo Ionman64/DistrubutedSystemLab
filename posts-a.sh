017caff8a55d41ee1e2af7362cdc0b516eb1947a
Henrik Numé <henriknume@gmail.com>
2017-12-14 14:44:05 +0100

    saving

--- posts-a.sh
+++ posts-a.sh
@@ -1,4 +1,4 @@
 for i in `seq 1 10`; do
-curl -d 'entry=A-'${i} -X 'POST' 'http://127.0.0.1:61001/board'
+curl -d 'entry=A-'${i} -X 'POST' 'http://10.1.0.1:61001/board'
 done
 #This will post entry=t1, entry=t2, ..., entry=t20 to http://ip:port/entries
