fc73335c8caf2c716b6ec993d6454cb4b4f1851c
Peter Pickerill <peterpickerill2016@gmail.com>
2017-12-14 13:33:51 +0100

    Changed the way stress test was done

--- lab1.py
+++ lab1.py
@@ -90,7 +90,7 @@ class Lab1():
 		#nbOfClientsPerRegion = 2
 		#nbOfRegions = 2
 
-		nbOfServersPerRegion = 2 # 5
+		nbOfServersPerRegion = 3 # 5
 		nbOfClientsPerRegion = 2 # 2
 		nbOfRegions = 2 # 2
 
