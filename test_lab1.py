3dfcb0758a294cabb8814c3f83080060e705b7a7
Peter Pickerill <peterpickerill2016@gmail.com>
2017-12-14 13:23:16 +0100

    Changed the way stress test was done

--- test_lab1.py
+++ test_lab1.py
@@ -40,8 +40,8 @@ class SimpleTestCase(unittest.TestCase):
         NUMBER_OF_MESSAGES = 50
         success = True
         for k in range(NUMBER_OF_MESSAGES):
-            ip_address = "http://10.1.0.%s:61001/board" % str(i+1)
             for i in range(NUMBER_OF_NODES):
+                ip_address = "http://10.1.0.%s:61001/board" % str(i+1)
                 r = requests.post(ip_address, data={'entry': "I am %s sending %i" % (ip_address, i)})
             if success:
                 success = (r.status_code == 200)
