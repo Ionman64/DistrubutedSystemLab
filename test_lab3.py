c392cb2553a0cc4a679269ce27af5b2132d9cc00
Henrik Numé <henriknume@gmail.com>
2017-12-14 15:20:34 +0100

    submission

--- test_lab3.py
+++ test_lab3.py
@@ -12,29 +12,6 @@ class SimpleTestCase(unittest.TestCase):
     def tearDown(self):
         """Call after every test case."""
 
-    ''' def testBoard(self):
-        r = requests.get("http://10.1.0.1:61001/board")
-        assert r.status_code == 200
-    def testEntryAndModify(self):
-        ENTRY_1 = "banana"
-        r = requests.post("http://10.1.0.1:61001/board", data={'entry': ENTRY_1})
-        data = json.loads(r.text)
-        POST_ID = data["id"]
-        assert r.status_code == 200
-        #modify
-        ENTRY_1 = "apple"
-        r = requests.post("http://10.1.0.1:61001/board" + "/" + POST_ID, data={'entry': ENTRY_1})
-        assert r.status_code == 200
-    def testEntry(self):
-        # Posting an entry to board check if it exists:
-        ENTRY_1 = "banana"
-        r = requests.post("http://10.1.0.1:61001/board", data={'entry': ENTRY_1})
-        assert r.status_code == 200
-    def testDelete(self):
-        r = requests.post("http://10.1.0.1:61001/board", data={'entry': "banana"})
-        data = json.loads(r.text)
-        POST_ID = data["id"]
-        assert r.status_code == 200 '''
     def testStressTest(self):
         NUMBER_OF_NODES = 8
         NUMBER_OF_MESSAGES = 50
@@ -46,10 +23,10 @@ class SimpleTestCase(unittest.TestCase):
             if success:
                 success = (r.status_code == 200)
         assert success
-         
+
 
         #r = requests.delete("http://10.1.0.1:61001/board/%s" % POST_ID)
         #assert r.status_code == 200
-        
+
 if __name__ == "__main__":
     unittest.main() # run all tests
