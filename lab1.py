b3e6e24fc5c2bce9a4dd19975903e2f22b5b9162
Peter Pickerill <peterpickerill2016@gmail.com>
2017-12-14 09:56:24 +0100

    Added stress test to unit tests

--- lab1.py
+++ lab1.py
@@ -90,8 +90,8 @@ class Lab1():
 		#nbOfClientsPerRegion = 2
 		#nbOfRegions = 2
 
-		nbOfServersPerRegion = 3 # 5
-		nbOfClientsPerRegion = 1 # 2
+		nbOfServersPerRegion = 2 # 5
+		nbOfClientsPerRegion = 2 # 2
 		nbOfRegions = 1 # 2
 
 		localJitter = 10 # ms, the evolution of the time between two consecutive packets
