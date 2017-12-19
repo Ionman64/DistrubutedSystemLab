cf202e23e42d9dec9b22d080c3ccba3a4f6fc4da
Peter Pickerill <peterpickerill2016@gmail.com>
2017-12-19 13:59:36 +0100

    added vector propigation

--- server/server.py
+++ server/server.py
@@ -43,7 +43,9 @@ class BlackboardServer(HTTPServer):
         # Create vector clock and initalize all to 0
         self.vclock = dict.fromkeys(self.vessels, 0)
         self.byzantine_votes = {}
+        self.vector_byzantine_votes = {}
         self.isByzantineNode = False
+        self.round = 0
 
     def tick(self):
         this_vessel = self.get_ip_address()
@@ -216,13 +218,11 @@ class BlackboardRequestHandler(BaseHTTPRequestHandler):
             self.server.byzantine_votes[self.server.get_ip_address()] = True
             self.retransmit("/vote", "POST", self.server.get_ip_address(), "True")
             self.success_out()
-            return
         if request_path == "/vote/retreat":
             print "I am voting to retreat"
             self.server.byzantine_votes[self.server.get_ip_address()] = False
             self.retransmit("/vote", "POST", self.server.get_ip_address(), "False")
             self.success_out()
-            return
         if request_path == "/vote/byzantine":
             print "I am voting to byzantine"
             self.server.isByzantineNode = True
@@ -234,7 +234,6 @@ class BlackboardRequestHandler(BaseHTTPRequestHandler):
                 #else:
                 #    self.retransmit("/vote", "POST", self.server.get_ip_address(), "False")
             self.success_out()
-            return
 
         if request_path == "/propagate/vote":
             id_sender = parameters['id'][0]
@@ -249,13 +248,49 @@ class BlackboardRequestHandler(BaseHTTPRequestHandler):
             else:
                 # its a dict
                 "Receiving vote array"
-                self.server.byzantine_votes[id_sender] = json.loads(value)
-
+                self.server.vector_byzantine_votes[id_sender] = json.loads(value)
+        
+        if self.isRoundOne() and self.server.round == 0:
+            print "It's Round One"
             print "byz_votes: %s" % self.server.byzantine_votes
+            self.server.vector_byzantine_votes[self.server.get_ip_address()] = self.server.byzantine_votes
+            self.retransmit("/vote", "POST", self.server.get_ip_address(), json.dumps(self.server.byzantine_votes))
+            self.server.round = 1
+        if self.isRoundTwo() and self.server.round == 1:
+            print "It's Round Two"
+            print "byz_votes: %s" % self.server.vector_byzantine_votes
+            self.server.round = 2
+            #decide on plan
+            
+        #if self.server.isByzantineNode:
+        #    vote = self.compute_byzantine_vote_round1(3, True) 
+        #else:
+            
 
     def has_all_votes(self):
          return len(self.server.byzantine_votes) == len(self.server.vessels)
 
+    def has_all_vector_votes(self):
+         return len(self.server.vector_byzantine_votes) == len(self.server.vessels)
+
+    def isRoundOne(self):
+        if not self.has_all_votes():
+           return False 
+        success = True
+        for key in self.server.byzantine_votes:
+            if type(self.server.byzantine_votes[key]) != type(True):
+                success = False
+        return success
+
+    def isRoundTwo(self):
+        if not self.has_all_vector_votes():
+               return False 
+        success = True
+        for key in self.server.vector_byzantine_votes:
+            if type(self.server.vector_byzantine_votes[key]) != type({}):
+                success = False
+        return success
+                
 
     def success_out(self):
             self.set_HTTP_headers(200)
